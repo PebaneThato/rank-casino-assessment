53083f41a6b31ae19f8a188186ef75fcb1ef8379
Thato Pebane <jpebane@gmail.com>
2022-05-08 21:06:25 +0200

    Project first files

---
+++ src/main/java/com/rank/casinoassessment/CasinoAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.rank.casinoassessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasinoAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasinoAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rank/casinoassessment/CasinoAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rank.casinoassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasinoAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
